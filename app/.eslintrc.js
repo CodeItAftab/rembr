12cf6eb09936b13583ea38ebeb2277c0b954773a
Aftab Alam <aftabalamdlm@gmail.com>
2026-08-16 00:26:09 +0530

    fix: migrate ESLint config to flat config format for ESLint v9 compatibility

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,3 +1,8 @@
-module.exports = {
-  extends: "expo",
-};
+const expoConfig = require("eslint-config-expo/flat");
+
+module.exports = [
+  ...expoConfig,
+  {
+    ignores: ["dist/*", "node_modules/*", ".expo/*"],
+  },
+];
